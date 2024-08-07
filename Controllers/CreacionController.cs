be1110068e861d7e1805f454d089d996d76d716f
Eduardo Arroyo <aeduardoiran@gmail.com>
2024-08-06 17:40:17 -0700

    Version 1.0

---
+++ Controllers/CreacionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Pruebas.Controllers;
+
+public class CreacionController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
