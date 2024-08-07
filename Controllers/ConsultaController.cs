ef3106b407b646b6df9c0b9e8e8305fcdd0bedf1
Eduardo Arroyo <aeduardoiran@gmail.com>
2024-08-06 21:48:23 -0700

    Consulta controller

--- Controllers/ConsultaController.cs
+++ Controllers/ConsultaController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using System.Text.Json;
+using API_Pruebas.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API_Pruebas.Controllers;
 
@@ -13,6 +16,42 @@ public class ConsultaController : Controller
 
     public IActionResult Index()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostConsulta(ConsultaModel consulta)
+    {
+
+        var url = "https://arieswebapi-pre-produccion.azurewebsites.net/Contrato/rest/ObtenerContratoLote ";
+
+        var data = new
+        {
+            userName = consulta.userName,
+            password = consulta.password,
+            request = new
+            {
+                IdConpre = consulta.IdConpre
+            }
+        };
+        
+        var jsonString = JsonSerializer.Serialize(data);
+        var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+        var client = _clientFactory.CreateClient();
+        var response = await client.PostAsync(url, content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var responseContent = await response.Content.ReadAsStringAsync();
+            ViewBag.ResponseContent = responseContent;
+            Console.WriteLine(responseContent);
+        }
+        else
+        {
+            ViewBag.ResponseContent = $"Error {response.Content} - {response.ReasonPhrase}";
+        }
+        
         return View();
     }
 }
---
+++ Models/ConsultaModel.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Pruebas.Models;
+
+public class ConsultaModel
+{
+    public string userName { get; set; }
+    public string password { get; set; }
+    public string IdConpre { get; set; }
+}
