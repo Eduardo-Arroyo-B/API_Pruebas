7c662fe50216d08d0710c13da8e0e47a13567689
Eduardo Arroyo <aeduardoiran@gmail.com>
2024-08-06 18:08:10 -0700

    Creacion Modelo

--- Controllers/CreacionController.cs
+++ Controllers/CreacionController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API_Pruebas.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API_Pruebas.Controllers;
 
 public class CreacionController : Controller
 {
+
+    private readonly IHttpClientFactory _clientFactory;
+
+    public CreacionController(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
     public IActionResult Index()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostData(CreacionModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var url = "https://arieswebapi-pre-produccion.azurewebsites.net/Contrato/rest/CrearContratoLote_MP_PC ";
+
+            var data = new
+            {
+                userName = model.userName
+            };
+        }
+
         return View();
     }
 }
---
+++ Models/CreacionModel.cs
@@ -0,0 +1,16 @@
+﻿namespace API_Pruebas.Models;
+
+public class CreacionModel
+{
+    public string userName { get; set; }
+    public string password { get; set; }
+    public string Expediente { get; set; }
+    public string IdCte { get; set; }
+    public string IdConjunto { get; set; }
+    public string TipoContrato { get; set; }
+    public string FechaElaCto { get; set; }
+    public string InicioMantenimiento { get; set; }
+    public string Manzana { get; set; }
+    public string Lote { get; set; }
+}
+
